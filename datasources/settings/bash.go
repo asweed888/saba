22520a49b5b945b8851c49fb5bcab69f95ae038b
inadati <dr.inadati@gmail.com>
2023-06-05 13:37:16 +0900

    python,v,bashを追加 (#11)

--- datasources/settings/bash.go
+++ datasources/settings/bash.go
@@ -1,1 +1,12 @@
 package settings
+
+import "github.com/asweed888/saba/domain/model"
+
+
+
+var Bash = &model.TacitSetting{
+    FileModeStr: "0755",
+    TacitFileContents: func(c *model.TacitSetting, path string, fname string) string {
+        return "#!/bin/bash"
+    },
+}
--- datasources/settings/v.go
+++ datasources/settings/v.go
@@ -1,1 +1,9 @@
 package settings
+
+import "github.com/asweed888/saba/domain/model"
+
+
+var Vlang = &model.TacitSetting{
+    Ext: "v",
+    FileModeStr: "0644",
+}
