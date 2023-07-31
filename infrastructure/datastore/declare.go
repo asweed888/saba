25521698e4d4fca5a930cc0dcc314c00d63c457b
inadati <dr.inadati@gmail.com>
2023-07-31 18:24:02 +0900

    対応言語にjavascriptを追加 (#23)

--- infrastructure/datastore/declare.go
+++ infrastructure/datastore/declare.go
@@ -1,19 +1,19 @@
 package datastore
 
 import (
-	"errors"
-	"io/ioutil"
+    "errors"
+    "io/ioutil"
 
-	"github.com/asweed888/saba/datasources/settings"
-	"github.com/asweed888/saba/domain/model"
-	"github.com/asweed888/saba/domain/repository"
-	"github.com/asweed888/saba/utils"
-	"gopkg.in/yaml.v2"
+    "github.com/asweed888/saba/datasources/settings"
+    "github.com/asweed888/saba/domain/model"
+    "github.com/asweed888/saba/domain/repository"
+    "github.com/asweed888/saba/utils"
+    "gopkg.in/yaml.v2"
 )
 
 
 type declareRepository struct {
-	DeclareFile string
+    DeclareFile string
 }
 
 func NewDeclareRepository(declareFile string) repository.DeclareRepository {
@@ -23,47 +23,49 @@ func NewDeclareRepository(declareFile string) repository.DeclareRepository {
 func (r *declareRepository) Load() (*model.Declare, error) {
 
 
-	// 宣言ファイルを開く
+    // 宣言ファイルを開く
     f, err := ioutil.ReadFile(r.DeclareFile)
-	if err != nil {
-		return nil, utils.Error(err)
-	}
+    if err != nil {
+        return nil, utils.Error(err)
+    }
 
-	// 読み込んだ宣言ファイルをDeclare構造体に注入
-	var declare *model.Declare
-	err = yaml.Unmarshal(f, &declare)
-	if err != nil {
-		return nil, utils.Error(err)
-	}
+    // 読み込んだ宣言ファイルをDeclare構造体に注入
+    var declare *model.Declare
+    err = yaml.Unmarshal(f, &declare)
+    if err != nil {
+        return nil, utils.Error(err)
+    }
 
-	// 暗黙の設定を読み込む
-	setting, err := loadTacitSetting(declare.Lang)
-	if err != nil {
-		return nil, utils.Error(err)
-	}
+    // 暗黙の設定を読み込む
+    setting, err := loadTacitSetting(declare.Lang)
+    if err != nil {
+        return nil, utils.Error(err)
+    }
 
-	setting.Lang = declare.Lang
-	setting.Arch = declare.Arch
-	declare.TacitSetting = setting
+    setting.Lang = declare.Lang
+    setting.Arch = declare.Arch
+    declare.TacitSetting = setting
 
-	return declare, nil
+    return declare, nil
 }
 
 
 func loadTacitSetting(lang string) (*model.TacitSetting, error) {
-	switch lang {
-	case "go":
-		return settings.Golang, nil
-	case "rust":
-		return settings.Rust, nil
-	case "v":
-		return settings.Vlang, nil
-	case "python":
-		return settings.Python, nil
-	case "bash":
-		return settings.Bash, nil
-	default:
-		err := errors.New("Invalid programming language specified")
-		return nil, utils.Error(err)
-	}
+    switch lang {
+    case "go":
+        return settings.Golang, nil
+    case "rust":
+        return settings.Rust, nil
+    case "v":
+        return settings.Vlang, nil
+    case "python":
+        return settings.Python, nil
+    case "bash":
+        return settings.Bash, nil
+    case "javascript":
+        return settings.Javascript, nil
+    default:
+        err := errors.New("Invalid programming language specified")
+        return nil, utils.Error(err)
+    }
 }
--- presentation/command/handler/newCmd.go
+++ presentation/command/handler/newCmd.go
@@ -1,16 +1,16 @@
 package handler
 
 import (
-	"fmt"
+    "fmt"
 
-	"github.com/asweed888/saba/usecase"
-	"github.com/asweed888/saba/utils"
-	"github.com/manifoldco/promptui"
-	"github.com/spf13/cobra"
+    "github.com/asweed888/saba/usecase"
+    "github.com/asweed888/saba/utils"
+    "github.com/manifoldco/promptui"
+    "github.com/spf13/cobra"
 )
 
 type NewCmdHandler interface {
-	NewCmd() *cobra.Command
+    NewCmd() *cobra.Command
 }
 
 type newCmdHandler struct {
@@ -22,55 +22,57 @@ func NewNewCmdHandler(u usecase.InitialDeclareUseCase) NewCmdHandler {
 }
 
 func (h *newCmdHandler) NewCmd() *cobra.Command {
-	return &cobra.Command{
-		Use: "new",
-		Short: "Generate saba.yml.",
-		RunE: func(cmd *cobra.Command, args []string) error {
+    return &cobra.Command{
+        Use: "new",
+        Short: "Generate saba.yml.",
+        RunE: func(cmd *cobra.Command, args []string) error {
 
-			prompt := promptui.Select{
-				Label: "Please select a programming language.",
-				Items: []string{
-					"go",
-					"rust",
-					"python",
-					"v",
-					"bash",
-				},
-			}
+            prompt := promptui.Select{
+                Label: "Please select a programming language.",
+                Items: []string{
+                    "rust",
+                    "javascript",
+                    "go",
+                    "python",
+                    "bash",
+                    "v",
+                },
+                Size:      6,
+            }
 
-			_, lang, err := prompt.Run()
-			if err != nil {
-				return utils.Error(err)
-			}
+            _, lang, err := prompt.Run()
+            if err != nil {
+                return utils.Error(err)
+            }
 
-			var isDDD string
-			if lang == "bash" {
-				isDDD = "No"
-			} else {
-				prompt = promptui.Select{
-					Label: "Do you want to develop applications with ddd (onion architecture)?",
-					Items: []string{
-						"No",
-						"Yes",
-					},
-				}
+            var isDDD string
+            if lang == "bash" {
+                isDDD = "No"
+            } else {
+                prompt = promptui.Select{
+                    Label: "Do you want to develop applications with ddd (onion architecture)?",
+                    Items: []string{
+                        "No",
+                        "Yes",
+                    },
+                }
 
-				_, is_ddd, err := prompt.Run()
-				if err != nil {
-					return utils.Error(err)
-				}
-				isDDD = is_ddd
-			}
+                _, is_ddd, err := prompt.Run()
+                if err != nil {
+                    return utils.Error(err)
+                }
+                isDDD = is_ddd
+            }
 
-			fmt.Printf("Programming language to use → %q\n", lang)
-			fmt.Printf("Using ddd(onion architecture) → %q\n", isDDD)
+            fmt.Printf("Programming language to use → %q\n", lang)
+            fmt.Printf("Using ddd(onion architecture) → %q\n", isDDD)
 
-			err = h.GenerateInitialDeclareFile(lang, isDDD)
-			if err != nil {
-				return utils.Error(err)
-			}
+            err = h.GenerateInitialDeclareFile(lang, isDDD)
+            if err != nil {
+                return utils.Error(err)
+            }
 
-			return nil
-		},
-	}
+            return nil
+        },
+    }
 }
