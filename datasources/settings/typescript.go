6e6f624f01f176e11d0669648a128fa233dda6ac
inadati <dr.inadati@gmail.com>
2023-08-02 18:54:48 +0900

    typescriptのdddでのテンプレートを追加 (#34)

--- datasources/settings/typescript.go
+++ datasources/settings/typescript.go
@@ -2,8 +2,132 @@ package settings
 
 import "github.com/asweed888/saba/domain/model"
 
+type tsUtils struct {
+    utils
+}
 
 var Typescript = &model.TacitSetting{
     Ext: "ts",
     FileModeStr: "0644",
+    TacitFileContents: func(s *model.TacitSetting, path string, fname string) string {
+        ut := &tsUtils{utils{s, path, fname}}
+
+        if ut.IsDomainModel() {
+            return ut.DomainModelFileContents()
+        } else if ut.IsDomainRepository() {
+            return ut.DomainRepositoryFileContents()
+        } else if ut.IsInfra() {
+            return ut.InfraFileContents()
+        } else if ut.IsUseCase() {
+            return ut.UseCaseFileContents()
+        } else if ut.IsPresentation() {
+            return ut.PresentationFileContents()
+        } else if ut.IsDi() {
+            return ut.DiFileContents()
+        } else {
+            return ut.DefaultFileContents()
+        }
+    },
+}
+
+// ドメインモデルのファイルの場合に書き出す内容
+func (u *tsUtils) DomainModelFileContents() string {
+    tmpl := `export class {{ .Fname | ToTitle }} {
+  constructor() {}
+}
+`
+
+    return u.utils.CodeFileContents(tmpl)
+}
+
+
+// ドメインリポジトリのファイルの場合に書き出す内容
+func (u *tsUtils) DomainRepositoryFileContents() string {
+    tmpl := `import { {{ .Fname | ToTitle }} } from '../model/{{ .Fname }}'
+
+export interface I{{ .Fname | ToTitle }}Repository {}
+`
+
+    return u.utils.CodeFileContents(tmpl)
+}
+
+
+// インフラストラクチャ層のファイルの場合に書き出す内容
+func (u *tsUtils) InfraFileContents() string {
+    tmpl := `import { {{ .Fname | ToTitle }} } from '../../domain/model/{{ .Fname }}'
+import { I{{ .Fname | ToTitle }}Repository } from '../../domain/repository/{{ .Fname }}'
+
+export class {{ .Fname | ToTitle }}Repository implements I{{ .Fname | ToTitle }}Repository {
+  constructor() {}
+}
+`
+
+    return u.utils.CodeFileContents(tmpl)
+}
+
+
+// ユースケース層のファイルの場合に書き出す内容
+func (u *tsUtils) UseCaseFileContents() string {
+    tmpl := `import { {{ .Fname | ToTitle }} } from '../domain/model/{{ .Fname }}'
+import { I{{ .Fname | ToTitle }}Repository } from '../domain/repository/{{ .Fname }}'
+
+export interface I{{ .Fname | ToTitle }}UseCase {}
+
+export class {{ .Fname | ToTitle }}UseCase implements I{{ .Fname | ToTitle }}UseCase {
+  repository: I{{ .Fname | ToTitle }}Repository
+
+  constructor(repository: I{{ .Fname | ToTitle }}Repository) {
+    this.repository = repository
+  }
+}
+`
+
+    return u.utils.CodeFileContents(tmpl)
+}
+
+
+// プレゼンテーション層のファイルの場合に書き出す内容
+func (u *tsUtils) PresentationFileContents() string {
+    tmpl := `import { {{ .Fname | ToTitle }} } from '../../../domain/model/{{ .Fname }}'
+import { I{{ .Fname | ToTitle }}UseCase } from '../../../usecase/{{ .Fname }}'
+
+export interface I{{ .Fname | ToTitle }}{{ .Pkgname | ToTitle }} {}
+
+export class {{ .Fname | ToTitle }}{{ .Pkgname | ToTitle }} implements I{{ .Fname | ToTitle }}{{ .Pkgname | ToTitle }} {
+  usecase: I{{ .Fname | ToTitle }}UseCase
+
+  constructor(usecase: I{{ .Fname | ToTitle }}UseCase) {
+    this.usecase = usecase
+  }
+}
+`
+
+    if u.Fname == "app" || u.Fname == "mod" || u.Setting.IsNoExtensionGroup(u.Fname) {
+        return u.DefaultFileContents()
+    } else {
+        return u.utils.CodeFileContents(tmpl)
+    }
+}
+
+
+// DIコンテナのファイルの場合に書き出す内容
+func (u *tsUtils) DiFileContents() string {
+    tmpl := `import {  } from '../domain/repository'
+import {  } from '../infrastructure/datastore'
+import {  } from '../presentation'
+
+interface IDIContainer {}
+
+export class DIContainer implements IDIContainer {
+  constructor() {}
+}
+`
+
+    return tmpl
+}
+
+// その他のファイルの場合に書き出す内容 (デフォルト)
+func (u *tsUtils) DefaultFileContents() string {
+    tmpl := ``
+    return tmpl
 }
--- domain/model/tacitSetting.go
+++ domain/model/tacitSetting.go
@@ -1,45 +1,50 @@
 package model
 
 import (
-	"fmt"
-	"io/fs"
-	"os"
-	"strconv"
+    "fmt"
+    "io/fs"
+    "os"
+    "strconv"
+    "strings"
 )
 
 type TacitSetting struct {
-	Lang              string
-	Arch              string
-	Ext               string
-	FileModeStr       string
-	TacitFileContents func(conf *TacitSetting, path string, fname string) string
-	TacitSubAction    func(decl *Declare) error
+    Lang              string
+    Arch              string
+    Ext               string
+    FileModeStr       string
+    TacitFileContents func(conf *TacitSetting, path string, fname string) string
+    TacitSubAction    func(decl *Declare) error
 }
 
 func (s *TacitSetting) FileMode() fs.FileMode {
-	perm32, _ := strconv.ParseUint(s.FileModeStr, 8, 32)
-	return os.FileMode(perm32)
+    perm32, _ := strconv.ParseUint(s.FileModeStr, 8, 32)
+    return os.FileMode(perm32)
 }
 
 func (s *TacitSetting) CodeFileFullPath(path string, fname string) string {
-	if s.Ext == "" {
-		return fmt.Sprintf("%s/%s", path, fname)
-	} else {
-		return fmt.Sprintf("%s/%s.%s", path, fname, s.Ext)
-	}
+    if s.Ext == "" || s.IsNoExtensionGroup(fname) {
+        return fmt.Sprintf("%s/%s", path, fname)
+    } else {
+        return fmt.Sprintf("%s/%s.%s", path, fname, s.Ext)
+    }
 }
 
 func (s *TacitSetting) FileContents(path string, fname string) string {
-	if s.TacitFileContents == nil {
-		return ""
-	}
-	return s.TacitFileContents(s, path, fname)
+    if s.TacitFileContents == nil {
+        return ""
+    }
+    return s.TacitFileContents(s, path, fname)
 }
 
 func (s *TacitSetting) SubAction(decl *Declare) error {
 
-	if s.TacitSubAction == nil {
-		return nil
-	}
-	return s.TacitSubAction(decl)
+    if s.TacitSubAction == nil {
+        return nil
+    }
+    return s.TacitSubAction(decl)
+}
+
+func (s *TacitSetting) IsNoExtensionGroup(fname string) bool {
+    return strings.Contains(fname, ".svelte") || strings.Contains(fname, ".tsx") || strings.Contains(fname, ".vue")
 }
