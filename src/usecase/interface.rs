049311f989b6f7164c803ec1ac4c601cecda98f4
inadati <dr.inadati@gmail.com>
2023-10-11 08:10:19 +0900

    共通関数とrustの実装を追加

--- src/usecase/interface.rs
+++ src/usecase/interface.rs
@@ -1,3 +1,15 @@
-pub trait CodeFileGenerator {
-    fn gen_file(&self);
+use crate::domain::model::manifest::Manifest;
+
+pub trait CodeFileGenerator<'a> {
+    fn gen_file(&self, repository: Manifest<'a>) {}
+    fn get_root_path(
+        &self,
+        repository: Manifest<'a>,
+        default_root: &str,
+    ) -> &str {
+        match repository.root {
+            "" => { default_root }
+            _ => { repository.root }
+        }
+    }
 }
--- src/usecase/rust_file.rs
+++ src/usecase/rust_file.rs
@@ -6,16 +6,19 @@ pub struct RustFileUseCase<'a> {
 }
 
 impl<'a> RustFileUseCase<'a> {
-    pub fn new(repository: Manifest) -> Self {
+    pub fn new(repository: Manifest<'a>) -> Self {
         Self{ repository }
     }
     pub fn generate_file(&self) {
-        self.gen_file()
+        self.gen_file(self.repository)
     }
 }
 
-impl<'a> CodeFileGenerator for RustFileUseCase<'a> {
-    fn gen_file(&self) {
+impl<'a> CodeFileGenerator<'a> for RustFileUseCase<'a> {
+    fn gen_file(&self, repository: Manifest<'a>) {
 
+        for spec in repository.spec {
+            spec["location"].as_str().unwrap();
+        }
     }
 }
