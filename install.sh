6cffb34876491aea9e5bf407a9bb016ecf60b2b5
inadati <dr.inadati@gmail.com>
2023-11-06 09:57:48 +0900

    no-cacheは意味ないことが分かったので削除 (#71)

--- install.sh
+++ install.sh
@@ -49,6 +49,6 @@ curl -L $INSTALL_TARGET_URL -o - | tar -xzvf - && mv ./saba $HOME_BIN
 
 alias_name="saba_install"
 if ! grep -q "$alias_name" "$BASHRC"; then
-    echo "alias $alias_name='curl -sSL -H \"Cache-Control: no-cache\" https://raw.githubusercontent.com/asweed888/saba/main/install.sh | bash && exec \$SHELL -l'" >> "$BASHRC"
+    echo "alias $alias_name='curl -sSL https://raw.githubusercontent.com/asweed888/saba/main/install.sh | bash && exec \$SHELL -l'" >> "$BASHRC"
     echo "[info] An alias for saba updates has been registered."
 fi
