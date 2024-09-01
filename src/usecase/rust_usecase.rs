f76c1753178ee28e43ccd6c10131ef84109b1eed
inadati <dr.inadati@gmail.com>
2024-09-01 13:31:15 +0900

    gen_upstream_postを実装

--- src/usecase/rust_usecase.rs
+++ src/usecase/rust_usecase.rs
@@ -5,12 +5,19 @@ use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 use regex::Regex;
+use anyhow::Context;
 
 pub struct Rust;
 
 impl Rust {
     pub fn new() -> anyhow::Result<Self> {
         Ok(Self{})
+    }
+    fn modblock_start(&self) -> String {
+        "// start auto exported by saba.\n".to_string()
+    }
+    fn modblock_end(&self) -> String {
+        "// end auto exported by saba.\n".to_string()
     }
     fn modblock_pattern(&self) -> anyhow::Result<Regex> {
         let pattern = r"// start auto exported by saba\.[\s\S]*// end auto exported by saba\.";
@@ -30,8 +37,31 @@ impl Rust {
             File::create(path.to_str().unwrap())?;
             path
         };
-        let main_rs = main_rs.file_name().unwrap().to_str().unwrap_or("");
-        Ok(main_rs.to_string())
+        let main_rs = main_rs
+            .file_name()
+            .unwrap()
+            .to_str()
+            .context("[ERROR] Failed to obtain main.rs or lib.rs information.");
+
+        main_rs
+    }
+    fn mod_rs(&self, wd: PathBuf) -> anyhow::Result<String> {
+        let workdir = wd.to_str().unwrap();
+        let mod_rs = if PathBuf::from(workdir.clone() + "mod.rs").exists() {
+            PathBuf::from(workdir.clone() + "mod.rs")
+        }
+        else {
+            let path = PathBuf::from(workdir.clone() + "mod.rs");
+            File::create(path.to_str().unwrap())?;
+            path
+        };
+        let mod_rs = mod_rs
+            .file_name()
+            .unwrap()
+            .to_str()
+            .context("[ERROR] Failed to obtain mod.rs information.");
+
+        mod_rs
     }
 }
 
@@ -47,7 +77,7 @@ impl codefile::Act for Rust {
         let regx = self.modblock_pattern()?;
 
         // 新しいmodblockを作成
-        let mut modblock = "// start auto exported by saba.\n".to_string();
+        let mut modblock = self.modblock_start();
         for entry in fs::read_dir(root)? {
             let entry = entry?;
             let path = entry.path();
@@ -57,11 +87,14 @@ impl codefile::Act for Rust {
             if file_name == main_rs.as_str() {
                 continue;
             }
-            else if file_stem != "" {
+            else if file_stem != "" && main_rs.as_str() == "main.rs" {
                 modblock = format!("{}mod {};\n", modblock, file_stem);
             }
+            else if file_stem != "" && main_rs.as_str() == "lib.rs" {
+                modblock = format!("{}pub mod {};\n", modblock, file_stem);
+            }
         }
-        modblock = format!("{}// end auto exported by saba.\n", modblock);
+        modblock = format!("{}{}", modblock, self.modblock_end());
 
         // 今のファイルの内容を読み込み
         file.read_to_string(&mut file_contents)?;
@@ -88,6 +121,62 @@ impl codefile::Act for Rust {
         Ok(())
     }
     fn gen_upstream_post(&self, wd: PathBuf) -> anyhow::Result<()> {
+        let workdir = wd.to_str().unwrap();
+        let mod_rs = self.mod_rs(wd.clone())?;
+        let mod_rs_path = PathBuf::from(workdir.clone() + mod_rs);
+        let mut file = File::open(mod_rs_path.clone())?;
+        let mut file_contents = String::new();
+        let regx = self.modblock_pattern()?;
+
+        let mut modblock = self.modblock_start();
+        for entry in fs::read_dir(workdir)? {
+            let entry = entry?;
+            let path = entry.path();
+
+            let file_name = path
+                .file_name()
+                .unwrap()
+                .to_str()
+                .expect("[ERROR] Failed to get file name.");
+
+            let file_stem = path
+                .file_stem()
+                .unwrap()
+                .to_str()
+                .expect("[ERROR] Failed to obtain file stem.");
+
+            if file_name == mod_rs.as_str() {
+                continue;
+            }
+            else {
+                modblock = format!("{}pub mod {};\n", modblock, file_stem);
+            }
+        }
+        modblock = format!("{}{}", modblock, self.modblock_end());
+
+        // 今のファイルの内容を読み込み
+        file.read_to_string(&mut file_contents)?;
+
+        // ファイル内にパターンが見つかった場合は置換
+        if regx.is_match(&file_contents) {
+            let replaced_contents = regx.replace_all(&file_contents, modblock);
+            let mut new_file = File::create(mod_rs_path)?;
+            new_file.write_all(replaced_contents.as_bytes())?;
+        }
+        // ファイル内にパターンが見つからなかった場合はmodblockをファイルの先頭に挿入
+        else {
+            let temp_file = mod_rs_path.with_extension("temp");
+            let mut new_file = File::create(&temp_file)?;
+            let new_modblock = format!("{}\n\n", modblock);
+
+            // 先頭にmodblockを挿入
+            new_file.write_all(new_modblock.as_bytes())?;
+            // 元のファイルの内容を挿入
+            new_file.write_all(file_contents.as_bytes())?;
+            fs::rename(&temp_file, mod_rs_path)?;
+        }
+
+
         Ok(())
     }
 }
