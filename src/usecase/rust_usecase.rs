82e96a66ec062ade8b61a8dc4b4b251a39d118a4
inadati <dr.inadati@gmail.com>
2024-10-21 12:50:04 +0900

    [Rust] lib.rsはpubになるよう再修正 (#150)

--- src/usecase/rust_usecase.rs
+++ src/usecase/rust_usecase.rs
@@ -156,13 +156,11 @@ impl<'a> CodefileAct<'a> for Rust<'a> {
             if file_name == main_rs.as_str() {
                 continue;
             }
-            else if file_stem != ""
-                && (
-                    main_rs.as_str() == "main.rs"
-                    || main_rs.as_str() == "lib.rs"
-                )
-            {
+            else if file_stem != "" && main_rs.as_str() == "main.rs" {
                 modblock = format!("{}mod {};\n", modblock, file_stem);
+            }
+            else if file_stem != "" && main_rs.as_str() == "lib.rs" {
+                modblock = format!("{}pub mod {};\n", modblock, file_stem);
             }
         }
         modblock = format!("{}{}", modblock, self.modblock_end());
